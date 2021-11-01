e8df592cee628abbc04f802a517af45d4dc193e3
Pavel Raiskup <praiskup@redhat.com>
2021-11-01 14:39:27 +0100

    Update copr-cli-krb-fallback.py

--- copr-cli-krb-fallback.py
+++ copr-cli-krb-fallback.py
@@ -6,14 +6,16 @@ token = None
 if config_file:
    # load instance (fedora/redhat/..), username, token, etc.
    ... = load_config_file()
-  
-if username is None:
-   username = pwname()
-   
+     
 if token is None:
+    # How do we know the username for e.g. 'dnf build <projectname> local.src.rpm'?
+    # Do we need it?
+   
+    # (temporarily?) notify that we use kerberos, can be opt-outed by config
     if not config.kerberos:
-        log.info("trying kerberos with %s", username)
-        log.info("silence this by kerberos = True in your config")
+        log.info("Trying kerberos with %s", username)
+        log.info("silence this by 'kerberos = True' in your config")
+ 
     # Go with kerberos log-in attempt
 else:
     # Go without kerberos here (old-code)
